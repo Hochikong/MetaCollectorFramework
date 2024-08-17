2e91aede0707130d1e9e2907814e3b314c790495
Hochikong <ckhoidea@hotmail.com>
2024-08-18 01:13:31 +0800

    add sleep support in downloader
    Signed-off-by: Hochikong <ckhoidea@hotmail.com>

--- MetaCollector/crawler/request_downloader.py
+++ MetaCollector/crawler/request_downloader.py
@@ -4,10 +4,11 @@
 # @FileName: request_downloader.py
 
 import concurrent.futures
+import time
 from typing import List
 
 from tqdm import tqdm
-
+from MetaCollector.base.utils.random import generate_random_float
 from MetaCollector.base.utils.network.request_builder import HttpRequestsMaker
 
 
@@ -21,19 +22,23 @@ class RequestDownloader(object):
         self.pool = concurrent.futures.ThreadPoolExecutor(max_workers=max_threads)
         self.timeout = max_timeout
 
-    def download(self, urls_with_path: List[tuple]):
+    def download(self, urls_with_path: List[tuple], sleep_in_seconds: tuple = None):
         """
         给一个list，list包含两个字段：url与目标名字进行下载
 
         :param urls_with_path: e.g. [('url'http://xxx.jpg', '/PATH_TO/xxx.jpg')]
+        :param sleep_in_seconds: 每下载一个文件的睡眠时间，例如：(1,3)，随机睡眠1~3秒
         :return:
         """
         self.progress = tqdm(total=len(urls_with_path))
-        concurrent.futures.wait([self.pool.submit(self.__download, down_dict)
+        concurrent.futures.wait([self.pool.submit(self.__download, down_dict, sleep_in_seconds)
                                  for down_dict in urls_with_path], return_when='ALL_COMPLETED')
         self.progress.close()
 
-    def __download(self, down_dict: tuple):
+    def __download(self, down_dict: tuple, sleep_in_seconds: tuple = None):
         data = self.hm.send_request(url=down_dict[0], timeout=self.timeout)
         self.hm.download_file(data.content, down_dict[1])
+        if sleep_in_seconds is not None:
+            t = generate_random_float(sleep_in_seconds[0], sleep_in_seconds[1])
+            time.sleep(t)
         self.progress.update(1)
